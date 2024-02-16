2c5cb3f45f6cf099673df12ccf9d8b72604c5ecd
Puert4 <jullian.puerta248239@potros.itson.edu.mx>
2024-02-16 15:11:36 -0800

    Creacion CajeroControl

---
+++ src/main/java/com/mycompany/cajerocontrol/CajeroControl.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cajerocontrol;
+
+/**
+ *
+ * @author TeLesheo
+ */
+public class CajeroControl {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
